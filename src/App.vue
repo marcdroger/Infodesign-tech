8e58830ac03313a0db7b916afb11ad749e452d7d
Marc Droger <marcdroger@hotmail.com>
2022-11-28 14:42:59 +0100

    Added description header + small fixes

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,9 @@
       <h1 class="fade-in-top">This storytelling project is best experienced<span>:</span></h1>
       <ul class="fade-in-top">
         <li>On a mid/large screen. (laptop/computer).</li>
-        <li>In a modern browser such as Chrome/Firefox.</li>
+        <li>In a modern browser such as Chrome/Firefox/Safari.</li>
         <li>While wearing headphones or sound on.</li>
-        <li>In fullscreen mode (press F11 on keyboard).</li>
+        <li>In fullscreen mode (press F11 on Windows/ ⌃⌘F on MacOs).</li>
       </ul>
       <p class="fade-in-bottom--second">All set?</p>
       <audio ref="audio__fade">
@@ -33,14 +33,14 @@
       <button class="fade-in-bottom--third" @click="hideOverlay">Dive Into History</button>
     </section>
 
-    <!-- TODO: prevent scroll on first overlay-->
+    <!-- TODO: prevent scroll on first overlay?-->
 
     <Nav />
     <main>
       <header>
         <div class="container">
           <h1 ref="header__title">The Best Games Ever Made<span>.</span></h1>
-          <p ref="header__paragraph">Dive into history.</p>
+          <p ref="header__paragraph">Let's go back in History. Searching for the best games ever made. From recent to old classics. Nostalgia will be delivered. <br /><span>Scroll down to start the journey</span></p>
         </div>
       </header>
       <ul class="games">
--- todo.md
+++ todo.md
@@ -5,12 +5,10 @@
 + Add final end section after 50 games + add interactive data d3?
 + add description header
 + Add scroll snap
-+ fix apple fullscreen browser (shortcut key)
 
 + add multiple consoles/platforms in json?
 
 ## Assignment
 
-+ Write readme with install instructions + banner + sources used
 + Document proces wiki
 + Sketchnotes (short summary) 3 guest lectures
